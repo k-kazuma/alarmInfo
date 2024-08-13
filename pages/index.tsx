d0bd69b014cf977d12258d3a607b9b1643ae88d7
k-kazuma <k_kazuma8@icloud.com>
2024-08-13 13:26:46 +0900

    フォーム途中

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,21 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const postAction = (e) => {
+  e.preventDefault();
+  console.log("hi");
+};
+
 export default function Home() {
-  return <div>aaa</div>;
+  return (
+    <>
+      <h1>ご意見・ご要望</h1>
+      <form onSubmit={postAction}>
+        <textarea name="textarea"></textarea>
+        <button type="submit">送信</button>
+      </form>
+    </>
+  );
 }
 
 //adfafaf
