ebab5354fd983212a8a15c5185d86b449022b0c2
k-kazuma <k_kazuma8@icloud.com>
2024-08-17 13:24:41 +0900

    State追加途中

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,45 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const postAction = (e: any) => {
+const postAction = (e: any, comment: String) => {
   e.preventDefault();
-  console.log("hi");
+  console.log(comment);
 };
 
 export default function Home() {
+  const [comment, setComment] = useState("");
+  const contents = ["不具合", "機能の追加希望", "機能の改修希望"];
   return (
-    <div className="h-screen">
-      <div className="text-center mt-5">
+    <div>
+      <div className="text-center mt-5 mx-auto">
         <h2>【ご意見・ご要望】</h2>
         <p className="p-4">
-          当アプリの不具合や、追加希望の機能、改修希望される内容など下記フォームより送信してください。
+          当アプリの不具合や、機能の追加、改修希望される内容など下記フォームより送信してください。
         </p>
       </div>
       <form
-        onSubmit={postAction}
-        className="w-screen flex items-center justify-center"
+        onSubmit={(e) => postAction(e, comment)}
+        className="flex justify-center items-center my-20"
       >
-        <div className=" w-screen ml-5 mr-5 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+        <div className="ml-5 mr-5 w-screen border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+          <select className=" bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+            {contents.map((content) => {
+              return <option value="">{content}</option>;
+            })}
+          </select>
           <div className="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
             <label className="sr-only">Your comment</label>
             <textarea
               id="comment"
               rows={7}
               className="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
-              placeholder="Write a comment..."
+              placeholder="こちらにご記入をお願いします..."
               required
+              value={comment} // 状態の値を反映
+              onChange={(e) => setComment(e.target.value)}
             ></textarea>
           </div>
           <div className="flex items-center px-3 py-2 border-t dark:border-gray-600">
