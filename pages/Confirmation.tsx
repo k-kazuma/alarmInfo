ec9ba596aa63323c0b626c4a90ad607442ddebbf
k-kazuma <k_kazuma8@icloud.com>
2024-08-25 14:13:20 +0900

    レイアウト修正中

--- pages/Confirmation.tsx
+++ pages/Confirmation.tsx
@@ -1,16 +1,28 @@
 import React from "react";
 import Header from "./Header";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const Confirmation = () => {
   const router = useRouter();
   const comment = router.query.comment;
+  const content = router.query.content;
+
+  const submit = () => {
+    console.log("click");
+    console.log(comment);
+    console.log(content);
+  };
+
   return (
     <>
       <Header />
-      <div>confirmation</div>
-      {comment == "" ? "問い合わせ内容を入力してください" : comment}
-      <p>{comment}</p>
+      <div>
+        <p>問合せ内容：{content}</p>
+        <p>{comment}</p>
+        <button onClick={submit}>送信する</button>
+        <Link href="/">戻る</Link>
+      </div>
     </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,8 +13,13 @@ const postAction = (e: any, comment: String, content: String) => {
 
 export default function Home() {
   const [comment, setComment] = useState("");
-  const [content, setContent] = useState("不具合");
-  const contents = ["不具合", "機能の追加希望", "機能の改修希望"];
+  const [content, setContent] = useState("選択してください");
+  const contents = [
+    "選択してください",
+    "不具合",
+    "機能の追加希望",
+    "機能の改修希望",
+  ];
 
   const handleContents = (e: any) => {
     setContent(e.target.value);
@@ -34,38 +39,43 @@ export default function Home() {
           onSubmit={(e) => postAction(e, comment, content)}
           className="flex justify-center items-center my-20"
         >
-          <div className="ml-5 mr-5 w-screen border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
-            <select
-              className="border bg-gray-50 dark:bg-gray-700 dark:border-gray-600"
-              onChange={handleContents}
-            >
-              {contents.map((content) => {
-                return <option key={content}>{content}</option>;
-              })}
-            </select>
-            <div className="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
+          <div className="ml-5 mr-5 w-screen">
+            <div className="flex">
+              <h3>問合せ内容：</h3>
+              <select className="" onChange={handleContents}>
+                {contents.map((content) => {
+                  return <option key={content}>{content}</option>;
+                })}
+              </select>
+            </div>
+            <div className="px-4 py-2">
               <label className="sr-only">Your comment</label>
               <textarea
                 id="comment"
                 rows={7}
-                className="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+                className="w-full px-0 text-sm"
                 placeholder="こちらにご記入をお願いします..."
                 value={comment} // 状態の値を反映
                 onChange={(e) => setComment(e.target.value)}
                 required
               ></textarea>
             </div>
-            <div className="flex items-center px-3 py-2 border-t dark:border-gray-600">
+            <div className="flex items-center px-3 py-2 border-t">
               <Link
                 href={{
                   pathname: `/Confirmation`,
                   query: {
                     comment: comment,
+                    content: content,
                   },
                 }}
                 content="aaa"
                 type="submit"
-                className=" mr-0 ml-auto inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
+                className={
+                  content != "選択してください" && comment != ""
+                    ? "button mr-0 ml-auto inline-flex items-center py-2.5 px-4 text-xs font-medium text-center rounded-lg"
+                    : "button mr-0 ml-auto inline-flex items-center py-2.5 px-4 text-xs font-medium text-center rounded-lg opacity-20 pointer-events-none"
+                }
               >
                 確認画面へ
               </Link>
