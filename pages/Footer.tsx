9876f909421122c48a0ca78c9da769f432754e53
k-kazuma <k_kazuma8@icloud.com>
2024-10-10 12:30:19 +0900

    top改修中

--- pages/Footer.tsx
+++ pages/Footer.tsx
@@ -3,9 +3,12 @@ import Link from "next/link";
 
 function Footer() {
   return (
-    <Link href={"/privacypolicy"} type="submit" className="text-slate-400">
-      プライバシーポリシー
-    </Link>
+    <div className="w-full absolute bottom-0 p-3 text-slate-400">
+      <Link href={"/privacypolicy"} type="submit" className="text-slate-400">
+        プライバシーポリシー
+      </Link>
+      <p className="text-center">2024.kazumakumagai</p>
+    </div>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,84 +7,26 @@ import Footer from "./Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const postAction = (e: any, comment: String, content: String) => {
-  e.preventDefault();
-  console.log(comment, content);
-};
-
 export default function Home() {
-  const [comment, setComment] = useState("");
-  const [content, setContent] = useState("選択してください");
-  const contents = [
-    "選択してください",
-    "不具合",
-    "機能の追加希望",
-    "機能の改修希望",
-  ];
-
-  const handleContents = (e: any) => {
-    setContent(e.target.value);
-  };
-
   return (
     <div className="max-h-screen overflow-hidden">
-      <Header />
       <div className="h-screen">
-        <div className="text-center mt-5 mx-auto">
-          <h2>【ご意見・ご要望】</h2>
-          <p className="p-4">
-            当アプリの不具合や、機能の追加、改修希望される内容など下記フォームより送信してください。
-          </p>
-        </div>
-        <form
-          onSubmit={(e) => postAction(e, comment, content)}
-          className="flex justify-center items-center my-10"
-        >
-          <div className="ml-5 mr-5 w-screen">
-            <div className="flex">
-              <h3>問合せ内容：</h3>
-              <select className="" onChange={handleContents}>
-                {contents.map((content) => {
-                  return <option key={content}>{content}</option>;
-                })}
-              </select>
-            </div>
-            <div className="px-4 py-2">
-              <label className="sr-only">Your comment</label>
-              <textarea
-                id="comment"
-                rows={7}
-                className="w-full px-0 text-sm"
-                placeholder="こちらにご記入をお願いします..."
-                value={comment} // 状態の値を反映
-                onChange={(e) => setComment(e.target.value)}
-                required
-              ></textarea>
-            </div>
-            <div className="flex items-center px-3 py-2 border-t">
-              <Link
-                href={{
-                  pathname: `/Confirmation`,
-                  query: {
-                    comment: comment,
-                    content: content,
-                  },
-                }}
-                content="aaa"
-                type="submit"
-                className={
-                  content != "選択してください" && comment != ""
-                    ? "button mr-0 ml-auto inline-flex items-center py-2.5 px-4 text-xs font-medium text-center rounded-lg"
-                    : "button mr-0 ml-auto inline-flex items-center py-2.5 px-4 text-xs font-medium text-center rounded-lg opacity-20 pointer-events-none"
-                }
-              >
-                確認画面へ
+        <Header />
+        <div className="flex">
+          <Image src="/image.png" width={100} height={100} alt="appIcon" />
+          <div>
+            <h1>AlarmScheduler</h1>
+            <button className="rounded-sm	bg-b">
+              <Link href="https://apps.apple.com/us/app/alarmscheduler/id6708228546">
+                ダウンロード
               </Link>
-              <div>{/* <Confirmation /> */}</div>
-              <div></div>
-            </div>
+            </button>
           </div>
-        </form>
+        </div>
+        <div>
+          <Link href="/inquiry">問合せフォームへ</Link>
+          <Link>メールで問合せ</Link>
+        </div>
         <Footer />
       </div>
     </div>
