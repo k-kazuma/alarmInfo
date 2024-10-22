3216c941ef51350129b4e004a7426779be03cb4a
k-kazuma <k_kazuma8@icloud.com>
2024-10-22 13:52:21 +0900

    form送信、ローディング追加

--- pages/Loading.tsx
+++ pages/Loading.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Loading() {
   return (
-    <div className="flex justify-center items-center gap-6 mt-10">
-      <div className="h-10 w-10 animate-spin border-[5px] border-sky-400 rounded-full  border-t-transparent"></div>
+    <div className="flex justify-center items-center gap-6 mt-72">
+      <div className="h-10 w-10 animate-spin border-[5px] border-orange-400 rounded-full  border-t-transparent"></div>
       <p className="text-[30px] font-weight">Loading</p>
     </div>
   );
