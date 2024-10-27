9a4a1e018c0c6c936767b7847e98382e1007c461
k-kazuma <k_kazuma8@icloud.com>
2024-10-27 13:23:49 +0900

    管理者用ページ作成

---
+++ pages/admin/DvRXnsnvc5MF/index.tsx
@@ -0,0 +1,52 @@
+import Header from "@/pages/Header";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+
+function index() {
+  const [idText, setIdText] = useState("");
+  const [pwText, setPwText] = useState("");
+  const router = useRouter();
+  const onChangeIdText = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    console.log(idText);
+    setIdText(event.target.value);
+  };
+
+  const onChangePwText = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setPwText(event.target.value);
+  };
+
+  const login = () => {
+    console.log(idText, pwText);
+    //ここで認証を行い認証できた場合問い合わせ一覧へ遷移
+
+    router.push({
+      pathname: "login/inquiriesList/",
+    });
+  };
+
+  return (
+    <>
+      <Header />
+      <div>
+        <h2>Login</h2>
+        <input
+          type="text"
+          value={idText}
+          onChange={onChangeIdText}
+          className="block text-center"
+        />
+        <input
+          type="text"
+          value={pwText}
+          onChange={onChangePwText}
+          className="block text-center"
+        />
+        <button onClick={login}>login</button>
+      </div>
+    </>
+  );
+}
+
+export default index;
+
+// [{"comment": "hihihihhiuh", "content": "不具合", "created": "Sat, 26 Oct 2024 03:17:01 GMT", "id": 1}]
---
+++ pages/admin/DvRXnsnvc5MF/inquiriesList/index.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+
+function index() {
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+  const [inquiriesList, setInquiriesList] = useState<object>({});
+
+  const getInquiriesList = async () => {
+    const res = await fetch(`${apiUrl}/`, {
+      method: "GET",
+    });
+
+    // resのステータスで条件分岐
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    } else {
+      const data = await res.json();
+      return data;
+    }
+  };
+
+  useEffect(() => {
+    setInquiriesList(getInquiriesList());
+    console.log(inquiriesList);
+  }, []);
+  return <div>index</div>;
+}
+
+export default index;
